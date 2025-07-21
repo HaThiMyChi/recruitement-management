f5d2db38b6be14d0c23e61dae3d26cf16c26071d
MyChi <mychikhtn@gmail.com>
2025-07-21 13:51:43 +0700

    update css navbar

--- src/components/navbar/NavbarComponent.tsx
+++ src/components/navbar/NavbarComponent.tsx
@@ -1,13 +1,19 @@
 import { Container, Nav, Navbar} from "react-bootstrap";
 import styles from "./NavbarComponent.module.css";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFire } from "@fortawesome/free-solid-svg-icons"
+
 
 const NavbarComponent: React.FC = () => {
     return (
        <Navbar bg="light" expand="lg">
         <Container>
             <Nav className="me-auto">
-                <Nav.Link as={Link} className={styles.navLink} to='/jobs'>Jobs</Nav.Link>
+                <Nav.Link as={Link} className={styles.navLink} style={{ color: ' rgb(221, 63, 36)' }} to='/jobs'>
+                    <FontAwesomeIcon style={{ paddingRight: '5px' }} className={styles.icon} icon={faFire} />
+                    Jobs
+                </Nav.Link>
             </Nav>
         </Container>
        </Navbar>
